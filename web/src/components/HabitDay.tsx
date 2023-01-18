616379963a2eec328f02db90a7fa955a448778a3
Valdir Silva <valdipiresba@gmail.com>
2023-01-18 14:23:30 -0300

    Component SummaryTable

---
+++ web/src/components/HabitDay.tsx
@@ -0,0 +1,14 @@
+// interface HabitDayProps {
+//     completed: number
+// }
+
+// Componente: Reaproveitar / isolar
+// Propriedade: Uma informação enviada para modificar um componente visual ou comportamentalmente
+
+export default function HabitDay() {
+    return (
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg">
+
+        </div>
+    )
+}
