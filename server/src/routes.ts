d1f11326c83818909e3d34f27f947811697ed468
Valdir Silva <valdipiresba@gmail.com>
2023-01-19 13:37:03 -0300

    creating route for completing habit

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -77,5 +77,59 @@ export async function appRoutes(app: FastifyInstance) {
             completedHabits,
         }
     })
+
+    // rota p/ completar não-completar um hábito
+    app.patch('/habits/:id/toggle', async (request) => {
+        // route param => parâmetro de identificação
+
+        const toggleHabitParams = z.object({
+            id: z.string().uuid(),
+        })
+
+        const { id } = toggleHabitParams.parse(request.params)
+
+        const today = dayjs().startOf('day').toDate()
+
+        let day = await prisma.day.findUnique({
+            where: {
+                date: today,
+            }
+        })
+
+        if (!day) {
+            day = await prisma.day.create({
+                data: {
+                    date: today,
+                }
+            })
+        }
+
+        const dayHabit = await prisma.dayHabit.findUnique({
+            where: {
+                day_id_habit_id: {
+                    day_id: day.id,
+                    habit_id: id,
+                }
+            }
+        })
+
+        if (dayHabit) {
+            // remover a marcação de completo
+            await prisma.dayHabit.delete({
+                where: {
+                    id: dayHabit.id
+                }
+            })
+        } else {
+            // Completa o hábito nesse dia
+            await prisma.dayHabit.create({
+                data: {
+                    day_id: day.id,
+                    habit_id: id,
+                }
+            })
+
+        }
+    })
 }
 
