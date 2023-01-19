4cad8760cc9ac5b1c7c50c8d1bc3044a02117a57
Valdir Silva <valdipiresba@gmail.com>
2023-01-19 16:27:30 -0300

    Refactoring components

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**.tsx',
+    './src/**/*.tsx',
     './index.html'
   ],
   theme: {
@@ -9,7 +9,12 @@ module.exports = {
       colors: {
         background: '#09090A'
       },
+
+      gridTemplateRows: {
+        7: 'repeat(7,minmax(0,1fr))',
+      }
     },
   },
   plugins: [],
+
 }
