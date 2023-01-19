b2c43a95080d8082ee4c193f2e6da726e9c661aa
Valdir Silva <valdipiresba@gmail.com>
2023-01-19 15:19:05 -0300

    Finish route summary day

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -131,5 +131,32 @@ export async function appRoutes(app: FastifyInstance) {
 
         }
     })
+
+    app.get('/summary', async (request) => {
+
+        const summary = await prisma.$queryRaw`
+            SELECT 
+                D.id, 
+                D.date,
+                (
+                    SELECT 
+                        cast(count(*) AS float)
+                    FROM day_habit DH
+                    WHERE DH.day_id = D.id    
+                ) AS completed, 
+                (
+                    SELECT
+                       cast(count(*) AS float)
+                    FROM habit_week_days HWD
+                    JOIN habits H 
+                        ON H.id = HWD.habit_id
+                    WHERE
+                        HWD.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') AS int)
+                        AND H.created_at <= D.date 
+                ) AS amount
+            FROM days D
+        `
+        return summary
+    })
 }
 
