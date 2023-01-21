1971f9440248663ce2e340551dc15ddee7a31c29
Valdir Silva <valdipiresba@gmail.com>
2023-01-21 14:52:22 -0300

    final adjustments

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -14,25 +14,27 @@ interface HabitDayProps {
 export default function HabitDay({ defaultCompleted = 0, amount = 0, date }: HabitDayProps) {
     const [completed, setCompleted] = useState(defaultCompleted)
 
-    const completedPercentage = amount > 0 ? Math.round((defaultCompleted / amount) * 100) : 0
+    const completedPercentage = amount > 0 ? Math.round((completed / amount) * 100) : 0
 
     const dayAndMonth = dayjs(date).format('DD/MM')
     const dayOfWeek = dayjs(date).format('dddd')
 
     function handleCompletedChanged(completed: number) {
         setCompleted(completed)
+
+        console.log(completed)
     }
 
     return (
         <Popover.Root>
             <Popover.Trigger
-                className={clsx('w-10 h-10  border-2  rounded-lg', {
+                className={clsx('w-10 h-10 border-2 rounded-lg transition-colors focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-background', {
                     'bg-zinc-900 border-zinc-800': completedPercentage === 0,
                     'bg-violet-900 border-violet-700': completedPercentage > 0 && completedPercentage < 20,
                     'bg-violet-800 border-violet-600': completedPercentage >= 20 && completedPercentage < 40,
                     'bg-violet-700 border-violet-500': completedPercentage >= 40 && completedPercentage < 60,
                     'bg-violet-600 border-violet-500': completedPercentage >= 60 && completedPercentage < 80,
-                    'bg-violet-500 border-violet-400': completedPercentage >= 80
+                    'bg-violet-500 border-violet-400': completedPercentage >= 80,
                 })}
             />
 
--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -77,7 +77,7 @@ export function NewHabitForm() {
                             onCheckedChange={() => handleToggleWeekDay(index)}
                             className="flex items-center gap-3 group "
                         >
-                            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+                            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors">
                                 <Checkbox.Indicator>
                                     <Check size={20} className="text-white" />
                                 </Checkbox.Indicator>
